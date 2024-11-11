c7190d2c8fdab2a9ee8ba179949e95b9d94bc794
pc <sakurovadaria@gmail.com>
2024-11-12 03:47:48 +0500

    First commit

---
+++ VarietyOfImplementations/src/main/java/implementations/Variety/VarietyOfImplementations/VarietyOfImplementationsApplication.java
@@ -0,0 +1,13 @@
+package implementations.Variety.VarietyOfImplementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarietyOfImplementationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarietyOfImplementationsApplication.class, args);
+	}
+
+}
